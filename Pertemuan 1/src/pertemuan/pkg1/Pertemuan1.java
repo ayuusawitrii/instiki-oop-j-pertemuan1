6238a4f2de648de5093754d27462e41983baa3e1
PC-F-18 <sawitridenpasar@gmail.com>
2025-03-04 21:38:48 +0800

    Pertemuan 1

---
+++ Pertemuan 1/src/pertemuan/pkg1/Pertemuan1.java
@@ -0,0 +1,16 @@
+package pertemuan.pkg1;
+
+/**
+ *
+ * @author Ayu Sawitri
+ */
+public class Pertemuan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Belajar Bahasa Java");
+    }
+    
+}
